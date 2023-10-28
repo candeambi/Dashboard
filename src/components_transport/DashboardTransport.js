061c064e59e9c7a70a56585c56d72b301f9b8989
candeambi <candeambi@gmail.com>
2023-10-28 11:06:18 -0300

    Agrego el selector de lineas de colectivo y muestra a traves del route_id

--- src/components_transport/DashboardTransport.js
+++ src/components_transport/DashboardTransport.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-//import LineSelector from './LineSelector';
 import DataTransport from './DataTransport.json';
 
 function DashboardTransport () {
 
+  const [selectedRoute, setSelectedRoute] = useState(null); // Estado para la línea seleccionada
+
+
     // Función para crear marcadores a partir de los datos del JSON
   const createMarkers = (data) => {
-    return data.map((DataTransport) => (
+    let filteredData = data;
+    if (selectedRoute) {
+      filteredData = data.filter((DataTransport) => DataTransport.route_id === selectedRoute);
+    }
+    return filteredData.map((DataTransport) => (
       <Marker
         key={DataTransport.id}
         position={[DataTransport.latitude, DataTransport.longitude]}
@@ -23,12 +29,21 @@ function DashboardTransport () {
 
   return (
     <div>
+      <select value={selectedRoute} onChange={(e) => setSelectedRoute(e.target.value || null)}>
+        <option value="">Selecciona una línea de colectivo</option>
+        {DataTransport.map((DataTransport) => (
+          <option key={DataTransport.route_id} value={DataTransport.route_id}>
+            {DataTransport.route_short_name}
+          </option>
+        ))}
+      </select>
+
     <MapContainer center={[-34.60376,  -58.38162]} zoom={10} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {createMarkers(DataTransport)}
+       {selectedRoute && createMarkers(DataTransport)}
     </MapContainer>
     </div>
   );
